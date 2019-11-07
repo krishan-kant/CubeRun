14f84a19a43f496e6942d93171ac81915148e42c
Krishan Kant <krish.kant.47@gmail.com>
2019-11-07 09:39:35 +0530

    assets uploaded

---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour{
+
+    public playerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Debug.Log(collisionInfo.collider.name);
+        if(collisionInfo.collider.tag == "obstacle")
+        {
+            //Debug.Log("We have hit an obstacle");
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().Endgame();
+        }
+    }
+}
