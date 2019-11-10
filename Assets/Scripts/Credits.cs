65a47ce463174b67c36db6fdc4ba92a7a96774f0
Krishan Kant <krish.kant.47@gmail.com>
2019-11-10 17:49:51 +0530

    Assets updated
    The levels have been remodelled and 2 more levels have been added. There are 6 levels now.

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ForwardForce = 2000f;
+    public float SideForce = 500f;
+    public float Accl = 12000f;
+    public float Daccl = 12000f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0,0,Accl * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0,0, -1 * Daccl * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-1 * SideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange );
+        }
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().Endgame();
+        }
+    }
+}
